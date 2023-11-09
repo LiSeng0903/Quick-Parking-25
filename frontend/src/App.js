5b7012de7b9433891af776b3b3d3281913366ea5
羅采蓁 <b09705059@ntu.edu.tw>
2023-11-09 11:29:01 +0800

    add Homepage

---
+++ frontend/src/App.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { ChakraProvider, Box, Text, Link, VStack, Code, Grid, theme, Heading, HStack, Button, Stack } from '@chakra-ui/react';
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider, Link as ReactRouterLink, NavLink } from 'react-router-dom'
+import { ColorModeSwitcher } from './ColorModeSwitcher';
+import Homepage from './Pages/Homepage.js';
+import { ArrowForwardIcon } from '@chakra-ui/icons';
+import { Logo } from './Logo';
+// import RootLayout from './Layouts/'
+// import { Link as ChakraLink, LinkProps } from '@chakra-ui/react'
+
+// router and routes
+const router = createBrowserRouter(
+    createRoutesFromElements(
+        <Route path="/">
+            <Route index element={<Homepage />} />
+            <Route path='home' element={<Homepage />} />
+        </Route>
+      
+    )
+)
+
+function App() {
+  return (
+    <ChakraProvider theme={theme}>
+      <Box textAlign="center" fontSize="xl">
+        <Grid maxH="80vh" p={0}>
+          <ColorModeSwitcher justifySelf="flex-end" m={5} />
+          <VStack>
+            <RouterProvider router={router} />
+          </VStack>
+        </Grid>
+      </Box>
+    </ChakraProvider>
+    
+  )
+}
+
+export default App;
