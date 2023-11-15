bdfbc3533e2609ad6757bd5e7389ca43958326e3
Aliston <changliseng0903@gmail.com>
2023-11-16 04:16:53 +0800

    route fix

--- backend/app.py
+++ backend/app.py
@@ -3,7 +3,7 @@ from flask import Flask, jsonify
 app = Flask(__name__)
 
 # 取得停車場概覽資料
-@app.route("api/parking/status")
+@app.route("/api/parking/status")
 def getParkingStatus():
     status = {
         "car": 200,
