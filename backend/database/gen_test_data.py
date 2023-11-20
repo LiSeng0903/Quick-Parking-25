711263df6215b18e1fd50367bcadc988703d381f
PuSung <sam90.hsu@gmail.com>
2023-11-21 00:58:38 +0800

    new gen data

--- backend/database/gen_test_data.py
+++ backend/database/gen_test_data.py
@@ -21,17 +21,28 @@ for i in range(180):
     space_info = {
         "space_id": "1" + str(i + 1).zfill(3),
         "occupied": False,
-        "type": "car",
+        "type": "",
         "floor": 1,
         "status": "OK",
         "history": [],
+        "zone": "",
     }
+
+    # Set type
     if i in range(120):
-        space_info["type"] = "scooter"
-    if i in range(120, 125):
-        space_info["type"] = "pregnant"
-    if i in range(125, 130):
+        space_info["type"] = "motor"
+    elif i in range(120, 130):
         space_info["type"] = "disabled"
+    elif i in range(130, 180):
+        space_info["type"] = "car"
+
+    # Set zone
+    if i in range(60):
+        space_info["zone"] = "A"
+    elif i in range(60, 120):
+        space_info["zone"] = "B"
+    elif i in range(60, 120):
+        space_info["zone"] = "C"
 
     collection.insert_one(space_info)
 
@@ -41,15 +52,25 @@ for floor in range(2, 6):
         space_info = space_info = {
             "space_id": str(floor) + str(i + 1).zfill(3),
             "occupied": False,
-            "type": "car",
+            "type": "",
             "floor": floor,
             "status": "OK",
             "history": [],
+            "zone": "",
         }
 
-        if i in range(5):
-            space_info["type"] = "pregnant"
-        if i in range(5, 10):
+        # Set type
+        if i in range(10):
             space_info["type"] = "disabled"
+        elif i in range(10, 120):
+            space_info["type"] = "car"
+
+        # Set zone
+        if i in range(40):
+            space_info["zone"] = "A"
+        elif i in range(40, 80):
+            space_info["zone"] = "B"
+        elif i in range(80, 120):
+            space_info["zone"] = "C"
 
         collection.insert_one(space_info)
