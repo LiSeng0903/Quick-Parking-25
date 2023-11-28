eb1b50ff126cd48c871fb7a4cbc4d19070c1c49c
yclee6262 <61281834+yclee6262@users.noreply.github.com>
2023-11-28 17:41:47 +0800

    fix backend docker build bug

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,6 @@
 import json
 from flask import Flask, jsonify
-from app.services.get_status import *
+from app.application.services import *
 
 app = Flask(__name__)
 
@@ -9,6 +9,10 @@ app = Flask(__name__)
 def getParkingStatus():
     status = get_parking_info()
     return json.dumps(status)
+
+@app.route("/api/parking/map/<int:floor>", methods=['GET'])
+def getFloorMap(floor):
+    return f"get floor {floor} map"
 
 if __name__ == "__main__":
     app.run(debug=True, port=8000)
