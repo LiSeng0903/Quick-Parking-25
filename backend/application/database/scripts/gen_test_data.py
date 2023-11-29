cd801830d7ab708c1c9f5bc0221570594db20a82
PuSung <sam90.hsu@gmail.com>
2023-11-30 01:33:57 +0800

    gen test data by interface, new version

--- backend/application/database/scripts/gen_test_data.py
+++ backend/application/database/scripts/gen_test_data.py
@@ -1,21 +1,26 @@
-import json
 import os
-
-import bcrypt
-import mongoengine
-from dotenv import load_dotenv
-from interface import MessageInterface, ParkingSpaceInterface
-from mongoengine import Document, DynamicDocument
-from schema import Manager, Message, ParkingSpace
+import sys
 
 cwd = os.path.dirname(os.path.abspath(__file__))
+interface_path = os.path.join(cwd, "../interfaces/")
+sys.path.append(interface_path)
+
+import json
+
+import bcrypt
+from connection import connect_to_db
+from ManagerInterface import ManagerInterface
+from MessageInterface import MessageInterface
+from mongoengine import Document, DynamicDocument
+from ParkingSpaceInterface import ParkingSpaceInterface
 
 
 def clear_all_data():
     """刪除 mongoDB 上的所有資料"""
 
-    document_classes = Document.__subclasses__()
+    connect_to_db()
 
+    document_classes = Document.__subclasses__()
     for document_class in document_classes:
         if issubclass(document_class, Document) and document_class not in [Document, DynamicDocument]:
             document_class.objects.delete()
@@ -24,96 +29,106 @@ def clear_all_data():
 def gen_parking_space_data():
     """產生停車格的測試資料"""
 
+    connect_to_db()
+
+    # 一樓
     for i in range(180):
-        type = ""
-        floor = 1
-        zone = ""
+        new_ps = {
+            "space_id": "1" + str(i + 1).zfill(3),
+            "space_type": "",
+            "floor": 1,
+            "zone": "",
+        }
 
         # Set type
         if i in range(120):
-            type = "motor"
+            new_ps["space_type"] = "motor"
         elif i in range(120, 130):
-            type = "disabled"
+            new_ps["space_type"] = "priority"
         elif i in range(130, 180):
-            type = "car"
+            new_ps["space_type"] = "car"
 
         # Set zone
         if i in range(60):
-            zone = "A"
+            new_ps["zone"] = "A"
         elif i in range(60, 120):
-            zone = "B"
-        elif i in range(120, 180):
-            zone = "C"
+            new_ps["zone"] = "B"
+        elif i in range(120, 140):
+            new_ps["zone"] = "C"
+        elif i in range(140, 160):
+            new_ps["zone"] = "D"
+        elif i in range(140, 160):
+            new_ps["zone"] = "E"
 
-        ParkingSpaceInterface.create_empty_ps(floor, type, zone)
+        ParkingSpaceInterface.create_ps(new_ps)
 
     # 二樓到五樓
     for floor in range(2, 6):
         for i in range(120):
-            type = ""
-            zone = ""
+            new_ps = {
+                "space_id": str(floor) + str(i + 1).zfill(3),
+                "space_type": "",
+                "floor": 1,
+                "zone": "",
+            }
 
             # Set type
             if i in range(10):
-                type = "disabled"
+                new_ps["space_type"] = "priority"
             elif i in range(10, 120):
-                type = "car"
+                new_ps["space_type"] = "car"
 
             # Set zone
             if i in range(40):
-                zone = "A"
+                new_ps["zone"] = "A"
             elif i in range(40, 80):
-                zone = "B"
+                new_ps["zone"] = "B"
             elif i in range(80, 120):
-                zone = "C"
+                new_ps["zone"] = "C"
 
-            ParkingSpaceInterface.create_empty_ps(floor, type, zone)
+            ParkingSpaceInterface.create_ps(new_ps)
 
 
 def gen_message_data():
     """產生提示訊息的資料"""
 
-    msgs = [Message(content="五樓整修中"), Message(content="四樓無障礙車位故障，請暫停使用")]
+    # 連接資料庫
+    connect_to_db()
 
+    # 取得所有公告訊息
+    msgs = ["五樓整修中", "四樓無障礙車位故障，請暫停使用"]
     for msg in msgs:
-        msg.save()
+        MessageInterface.create_message({"content": msg})
 
 
 def gen_manager_accounts():
     """產生管理員帳號"""
 
-    with open(cwd + "/managers.json", "r", encoding="utf-8") as f:
+    # 連接資料庫
+    connect_to_db()
+
+    with open(os.path.join(cwd, "../managers.json"), "r", encoding="utf-8") as f:
         managers = json.load(f)["managers"]
 
     for manager in managers:
-        m = Manager(
-            account=manager["account"],
-            password=bcrypt.hashpw(manager["password"].encode("utf-8"), bcrypt.gensalt()),
-            login=False,
-            name=manager["name"],
-        )
+        manager_dict = {
+            "account": manager["account"],
+            "password": bcrypt.hashpw(manager["password"].encode("utf-8"), bcrypt.gensalt()),
+            "name": manager["name"],
+        }
 
-        m.save()
+        ManagerInterface.create_manager(manager)
 
 
 def gen_all_data():
     """產生全部資料"""
 
-    clear_all_data()
     gen_parking_space_data()
     gen_message_data()
     gen_manager_accounts()
 
 
 if __name__ == "__main__":
-    load_dotenv()
-    username = os.environ.get("DB_USERNAME")
-    password = os.environ.get("DB_PASSWORD")
-
-    mongoengine.connect(
-        "QuickParking",
-        host=f"mongodb+srv://{username}:{password}@cluster0.hcp2app.mongodb.net/?retryWrites=true&w=majority",
-    )
-
-    gen_all_data()
-    # gen_manager_accounts()
+    clear_all_data()
+    # gen_all_data()
+    gen_manager_accounts()
