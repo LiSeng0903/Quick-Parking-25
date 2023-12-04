b967070d764487134689c9e4f4420e93ecf724c6
yclee6262 <61281834+yclee6262@users.noreply.github.com>
2023-12-04 17:36:12 +0800

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,8 @@
 import json
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 # from app.application.services import *
 from app.application.services.get_status import get_parking_info
-from app.database.db_functions import get_floor_map
+from app.database.db_functions import *
 
 app = Flask(__name__)
 
@@ -16,6 +16,14 @@ def getParkingStatus():
 def getFloorMap(floor):
     floor_data = get_floor_map(floor)
     return floor_data
+
+@app.route("/api/parking/car/enterNum", methods=['POST'])
+def carEnterNum():
+    data = request.json
+    space_Id = str(data['spaceId'])
+    car_Id = data['carId']
+    park(space_Id, car_Id)
+    return "carEnterNum"
 
 if __name__ == "__main__":
     app.run(debug=True, port=4000)
