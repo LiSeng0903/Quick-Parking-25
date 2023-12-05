279e5b93a5564df2f738234e9ce40b15e80a7d3b
PuSung <sam90.hsu@gmail.com>
2023-12-05 15:14:15 +0800

    add comments on db_funcs

--- backend/application/database/db_functions/parking_space_functions.py
+++ backend/application/database/db_functions/parking_space_functions.py
@@ -13,14 +13,27 @@ from ParkingSpaceInterface import ParkingSpaceInterface as PSI
 
 @connect_decorator
 def get_remain_space_cnt():
+    """
+    取得停車場剩餘空位數量
+
+    Args:
+        None
+    Returns:
+        dict: 停車場剩餘空位數量，內容為 {
+            "car": int,
+            "motor": int,
+            "priority": int,
+        }
+    """
+
+    # 取得所有停車位
     all_ps = PSI.read_all_ps()
 
+    # 計算剩餘空位數量
     remain_space_cnt = {"car": 0, "motor": 0, "priority": 0}
-
     for ps in all_ps:
         if ps["occupied"] == True:
             continue
-
         remain_space_cnt[ps["space_type"]] += 1
 
     return remain_space_cnt
@@ -28,7 +41,31 @@ def get_remain_space_cnt():
 
 @connect_decorator
 def get_parking_space_by_floor(floor: int):
-    spaces = {}
+    """
+    回傳該層樓的停車位資訊
+
+    Args:
+        floor (int): 樓層
+    Returns:
+        dict: 該樓層的停車位資訊。內容為 {
+            "A": [
+                {
+                    "space_id": str,
+                    "space_type": "car" || "motor" || "priority",
+                    "occupied": bool,
+                },
+                ...
+            ],
+            "B": [
+                ...
+            ],
+            ...
+        }
+    """
+
+    spaces = {}  # 停車位資訊
+
+    # 取得該樓層的所有停車位
     ps_of_floor = PSI.read_ps_by_floor(floor)
 
     for ps in ps_of_floor:
@@ -48,16 +85,31 @@ def get_parking_space_by_floor(floor: int):
 
 @connect_decorator
 def can_park(space_id: str, car_id: str):
-    # Check ps exist
+    """
+    檢查停車位 space_id 是否可以停入車輛 car_id
+    總共會檢查
+    1. 停車位 space_id 是否存在
+    2. 停車位 space_id 是否已被佔用
+    3. 車輛 car_id 是否已經停在別的停車位
+
+    Args:
+        space_id (str): 停車位 ID
+        car_id (str): 車輛 ID
+    Returns:
+        bool: 是否可以停入
+        str: 訊息
+    """
+
+    # 停車位 space_id 是否存在
     ps = PSI.read_ps_by_space_id(space_id)
     if ps == None:
         return False, f"停車位 {space_id} 不存在"
 
-    # Check ps is not occupied
+    # 停車位 space_id 是否已被佔用
     if ps["occupied"] == True:
         return False, f"停車位 {space_id} 已被佔用"
 
-    # Check car is not parked
+    # 車輛 car_id 是否已經停在別的停車位
     car_parked_ps = PSI.read_ps_by_current_car_id(car_id)
     if car_parked_ps != None:
         return False, f"車輛 {car_id} 已經停在停車位 {car_parked_ps['space_id']} 中"
@@ -67,13 +119,26 @@ def can_park(space_id: str, car_id: str):
 
 @connect_decorator
 def park_car(space_id: str, car_id: str):
-    # update current_car_id
+    """
+    停車，更新停車位資訊，會更新
+    1. current_car_id
+    2. occupied
+    3. history，新增一筆停車紀錄，尚未包含離開時間
+
+    Args:
+        space_id (str): 停車位 ID
+        car_id (str): 車輛 ID
+    Returns:
+        None
+    """
+
+    # 更新 current_car_id
     PSI.update_ps_current_car_id(space_id, car_id)
 
-    # update occcupied
+    # 更新 occcupied
     PSI.update_ps_occupied(space_id, True)
 
-    # update history
+    # 更新 history
     history = PSI.read_ps_history(space_id)
     history.append(
         {
