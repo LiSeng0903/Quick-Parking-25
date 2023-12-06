4d3f22c155870f7d634c7c82e445452a09f966f9
yclee6262 <61281834+yclee6262@users.noreply.github.com>
2023-12-06 21:58:24 +0800

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -17,7 +17,7 @@ def getParkingStatus():
 @app.route("/api/parking/map/<int:floor>", methods=['GET'])
 def getFloorMap(floor):
     floor_data = get_space_by_floor(floor)
-    return floor_data
+    return json.dumps(floor_data)
 
 @app.route("/api/car/enterNum", methods=['POST'])
 def parkCar():
@@ -35,13 +35,13 @@ def findCar():
     space_id = str(data['spaceId'])
     car_id = data['carId']
     info = find_car(space_id, car_id)
-    return info
+    return json.dumps(info)
 
 @app.route("/api/guard/check/<int:carSpace>", methods=['GET'])
 def checkCar(carSpace):
     spaceStr = str(carSpace)
     info = guard_get_space_info(spaceStr)
-    return info
+    return json.dumps(info)
 
 @app.route("/api/guard/login", methods=['POST'])
 def guardLogin():
@@ -49,12 +49,13 @@ def guardLogin():
     account = data['account']
     password = data['password']
     can_login, msg = guard_login(account, password)
-    return can_login, msg
+    response = jsonify({'success': can_login, 'message': msg})
+    return response
 
 @app.route("/api/guard/allFloors", methods=['GET'])
 def getAllFloors():
-    parking_infos = guard_get_parking_info()
-    return parking_infos
+    parking_info = guard_get_parking_info()
+    return json.dumps(parking_info)
 
 if __name__ == "__main__":
     app.run(debug=True, port=4000)
