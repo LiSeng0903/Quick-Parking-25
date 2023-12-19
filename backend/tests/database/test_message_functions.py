29f310585dcfac662a368fc7ad5516b034afcc87
zhiyuuuu <b09705038@ntu.edu.tw>
2023-12-19 15:50:56 +0800

    update unit test for message functions

--- backend/tests/database/test_message_functions.py
+++ backend/tests/database/test_message_functions.py
@@ -2,21 +2,9 @@ import pytest
 from unittest.mock import Mock, call, patch
 from application.database.db_functions.message_functions import get_all_message
 
-from functools import wraps
 
-def mock_decorator(*args, **kwargs):
-    """Decorate by doing nothing."""
-    def decorator(f):
-        @wraps(f)
-        def decorated_function(*args, **kwargs):
-            return f(*args, **kwargs)
-        return decorated_function
-    return decorator
-
-# PATCH THE DECORATOR HERE
-patch('application.database.db_functions.message_functions.connect_decorator', mock_decorator).start()
-
-def test_get_all_message():
+@patch("application.database.db_functions.message_functions.connect_decorator")
+def test_get_all_message(mock_decorator):
     mock_all_messages = [
         {"content": "message1"}, {"content": "message2"}, {"content": "message3"}
     ]
@@ -26,6 +14,12 @@ def test_get_all_message():
         return_value=mock_all_messages
     ):
         assert get_all_message() == (True, msg_list)
+        
+    with patch(
+        "application.database.db_functions.message_functions.MessageInterface.read_all_messages",
+        return_value=[]
+    ):
+        assert get_all_message() == (True, [])
     
     err_str = "testing errors"
     with patch(
