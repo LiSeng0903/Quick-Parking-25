c6949533098f0d82586727595fc2c97185e99a50
PuSung <sam90.hsu@gmail.com>
2023-12-21 02:58:29 +0800

    use backing datebase

--- backend/config/config.py
+++ backend/config/config.py
@@ -1,10 +1,14 @@
 import os
+
 from dotenv import load_dotenv
 
 load_dotenv()
 username = os.environ.get("DB_USERNAME")
 password = os.environ.get("DB_PASSWORD")
 
+
 class Config:
-    DB_URL = f"mongodb+srv://{username}:{password}@cluster0.hcp2app.mongodb.net/?retryWrites=true&w=majority"
+    # BACKING_DB_URL = f"mongodb+srv://{username}:{password}@cluster0.goqxm68.mongodb.net/?retryWrites=true&w=majority"
+    DB_URL = f"mongodb+srv://{username}:{password}@cluster0.goqxm68.mongodb.net/?retryWrites=true&w=majority"
+    # DB_URL = f"mongodb+srv://{username}:{password}@cluster0.hcp2app.mongodb.net/?retryWrites=true&w=majority"
     DB_NAME = "QuickParking"
